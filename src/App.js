4634ec3c8ec9fad5455baac86c25d8c819cf12e2
Raphael Isla <rpbisla0025@gmail.com>
2023-06-24 21:26:41 +0800

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import { TiArrowDownThick, TiArrowUpThick, TiMediaPlay, TiMediaPause, TiArrowRepeat } from "react-icons/ti";
+
+const handleClick = () => {
+  alert("clicked")
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='title'>25 + 5 Clock</h1>
+      <div className='length-control'>
+        <div>
+          <h3 id="break-label" className='fs'>Break Length</h3>
+          <div className='control'>
+            <TiArrowDownThick id='break-decrement' onClick={handleClick} className="arrow-icon" />
+            <div id='break-length' className='fs'>5</div>
+            <TiArrowUpThick id='break-increment' onClick={handleClick} className="arrow-icon" />
+          </div>
+        </div>
+        <div>
+          <h3 id='session-label' className='fs'>Session Length</h3>
+          <div className='control'>
+            <TiArrowDownThick id="session-decremnent" onClick={handleClick} className="arrow-icon" />
+            <div id='session-length' className='fs'>25</div>
+            <TiArrowUpThick id="session-increment" onClick={handleClick} className="arrow-icon" />
+          </div>
+        </div>
+      </div>
+      <div className='timer'>
+        <h3 id='timer-label' className='fs'>Session</h3>
+        <div id='timer-left' className='fs2'>25:00</div>
+      </div>
+      <div className='timer-control'>
+        <div id='start_stop' onClick={handleClick} className='controls'>
+          <div className='ai'>
+            <TiMediaPlay className='arrow-icon' />
+          </div>
+          <div className='ai'>
+            <TiMediaPause className='arrow-icon' />
+          </div>
+        </div>
+        <TiArrowRepeat id='reset' onClick={handleClick} className='arrow-icon ai2' />
+      </div>
+      <p><span>Designed and Coded by</span> <br/> Raphael Isla</p>
     </div>
   );
 }
